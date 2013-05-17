2ea15b004683d9832bd367f1cd569b587f8aab7b
johmoh <mar.lue@web.de>
2013-05-17 21:04:22 +0200

    add shell script for generating code documentation (script is untested)

---
+++ scripts/generate-code-documentation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+BASE_DIR=`dirname $0`
+ROOT_DIR=$BASE_DIR/..
+
+$ROOT_DIR/node_modules/bin/yuidoc --quiet --extension ".js" --exclude "_SYNCAPP,.deprecated,.prototyping,.idea,.git,doc,logs,node_modules,nppBackup,spec" -o ./docs ./
