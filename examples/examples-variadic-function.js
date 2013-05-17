aff52cb0d44551f13fa106a25b8710f4f751d5bf
johmoh <mar.lue@web.de>
2013-05-17 22:42:18 +0200

    add an example for using ArgueJS2 in a variadic function

---
+++ examples/examples-variadic-function.js
@@ -0,0 +1,76 @@
+var arguejs2 = require("../arguejs2");
+
+// ################################################################################################
+
+// a variadic function
+function formatText_TailDefault() {
+    var signature = [{ formatString: String }, { replacements: arguejs2.TAIL }]; // "replacements" are marked as "mandatory" because do not want to call formatText without replacements here
+    var _args = arguejs2.getArguments(signature, arguments);
+
+    if (_args.replacements === null)            { throw new Error("ArgueJS has a bug (case: null). This line should never be reached!"); }
+    if (_args.replacements === undefined)       { throw new Error("ArgueJS has a bug (case: undefined). This line should never be reached!"); }
+    if (!(_args.replacements instanceof Array)) { throw new Error("ArgueJS has a bug (case: no array). This line should never be reached!"); }
+    if (_args.replacements.length <= 0)         { throw new Error("ArgueJS has a bug (case: empty array). This line should never be reached!"); }
+
+    var resultingText = _args.formatString;
+    for (var i = 0; i < _args.replacements.length; ++i) {
+        resultingText = resultingText.replace("{" + (i+1) + "}", _args.replacements[i]);
+    }
+    return resultingText;
+}
+
+// a variadic function
+function formatText_TailWithParenthesizeTail() {
+    var signature = [{ formatString: String }, { replacements: {type: arguejs2.TAIL, parenthesizeTail: true}}]; // same same formatText_TailDefault but now, "parenthesizeTail" is explicitly set to "false"
+    var _args = arguejs2.getArguments(signature, arguments);
+
+    if (_args.replacements === null)            { throw new Error("ArgueJS has a bug (case: null). This line should never be reached!"); }
+    if (_args.replacements === undefined)       { throw new Error("ArgueJS has a bug (case: undefined). This line should never be reached!"); }
+    if (!(_args.replacements instanceof Array)) { throw new Error("ArgueJS has a bug (case: no array). This line should never be reached!"); }
+    if (_args.replacements.length <= 0)         { throw new Error("ArgueJS has a bug (case: empty array). This line should never be reached!"); }
+
+    var resultingText = _args.formatString;
+    for (var i = 0; i < _args.replacements.length; ++i) {
+        resultingText = resultingText.replace("{" + (i+1) + "}", _args.replacements[i]);
+    }
+    return resultingText;
+}
+
+
+// a variadic function
+function formatText_TailWithoutParenthesizeTail() {
+    var signature = [{ formatString: String }, { replacements: {type: arguejs2.TAIL, parenthesizeTail: false}}]; // same same formatText_TailDefault but now "parenthesizeTail" is explicitly set to "true"
+    var _args = arguejs2.getArguments(signature, arguments);
+
+    if (_args.replacements === null)            { throw new Error("ArgueJS has a bug (case: null). This line should never be reached!"); }
+    if (_args.replacements === undefined)       { throw new Error("ArgueJS has a bug (case: undefined). This line should never be reached!"); }
+    if (!(_args.replacements instanceof Array)) { throw new Error("ArgueJS has a bug (case: no array). This line should never be reached!"); }
+    if (_args.replacements.length <= 0)         { throw new Error("ArgueJS has a bug (case: empty array). This line should never be reached!"); }
+
+    var resultingText = _args.formatString;
+    for (var i = 0; i < _args.replacements.length; ++i) {
+        resultingText = resultingText.replace("{" + (i+1) + "}", _args.replacements[i]);
+    }
+    return resultingText;
+}
+
+console.log();
+console.log(                formatText_TailDefault("formatText_TailDefault with values                : {1} {2}!", "Hello", "World")); // OUTPUT: formatText_TailDefault with values                : Hello World!
+console.log(   formatText_TailWithParenthesizeTail("formatText_TailWithParenthesizeTail with values   : {1} {2}!", "Hello", "World")); // OUTPUT: formatText_TailWithParenthesizeTail with values   : Hello World!
+console.log(formatText_TailWithoutParenthesizeTail("formatText_TailWithoutParenthesizeTail with values: {1} {2}!", "Hello", "World")); // OUTPUT: formatText_TailWithoutParenthesizeTail with values: Hello World!
+
+console.log();
+console.log(                formatText_TailDefault("formatText_TailDefault with an array                : {1} {2}!", ["Hello", "World"])); // OUTPUT: formatText_TailDefault with an array                : Hello,World {2}!
+console.log(   formatText_TailWithParenthesizeTail("formatText_TailWithParenthesizeTail with an array   : {1} {2}!", ["Hello", "World"])); // OUTPUT: formatText_TailWithParenthesizeTail with an array   : Hello,World {2}!
+console.log(formatText_TailWithoutParenthesizeTail("formatText_TailWithoutParenthesizeTail with an array: {1} {2}!", ["Hello", "World"])); // OUTPUT: formatText_TailWithoutParenthesizeTail with an array: Hello World!
+
+console.log();
+try {
+    console.log(formatText_TailDefault("You should never ever see that line! Reason: The tail argument in formatText_TailDefault is defined as 'mandatory'!")); // OUTPUT: As expected, an exception was thrown! message=Incompatible function call: mandatory argument "replacements" has no value.
+}
+catch(_ex) {
+    console.log("As expected, an exception was thrown! message=" + _ex.message);
+}
+
+console.log();
+console.log(formatText_TailDefault("Values for the tail argument can be of any type including undefined and null, of course: {1} - {2}", undefined, null)); // OUTPUT: Values for the tail argument can be of any type including undefined and null, of course: undefined - null
